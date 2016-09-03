73ddb05e06df700652482222e536a5a98ec28534
liyang77 <yangli77@gmail.com>
2016-09-03 08:50:40 -0400

    call standardization functions. (missing implmentation of array,map)

--- core/src/main/scala/com/fuseinfo/ber/batch/Loader.scala
+++ core/src/main/scala/com/fuseinfo/ber/batch/Loader.scala
@@ -1,8 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.fuseinfo.ber.batch
 
 import com.fuseinfo.ber.external._
 import com.fuseinfo.ber.conf._
-import java.lang.reflect.Constructor
+import java.lang.reflect.{Field}
 import org.apache.spark.sql.{SQLContext, Row}
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -31,30 +50,43 @@ object Loader {
     //Step 3. Optionally persist RAW_DF to K/V store
     
     //Step 4. call Standardization functions to transform to STD_RDD <Long, STD_Entity>
-    val stdBaseName = berConf getValueByKey "std"
     val stdClassName = berConf getValueByKey "class.std"
-    val stdBaseObject = Class.forName(stdBaseName).newInstance().asInstanceOf[StdClass[Row]]
+    val stdClass = Class.forName(stdClassName)
+    val fields = stdClass.getDeclaredFields
+    val newMap = fields.map(field => field.getName -> field)(collection.breakOut): Map[String, Field]  
+
+    val stdBaseName = berConf getValueByKey "std"
+    val stdBaseInstance = Class.forName(stdBaseName).newInstance().asInstanceOf[StdClass]
     
     val stdFuncs = berConf getValuesByPrefix "std."
     val stdFuncObjs = stdFuncs.map(pair => {
       val eInd = pair._2.indexOf('=')
       val cInd = pair._2.indexOf(',', eInd)
       val className = if (cInd > 0) pair._2.substring(eInd+1, cInd) else pair._2.substring(eInd+1)
-      val stdInstance = Class.forName(className).newInstance().asInstanceOf[StdClass[AnyRef]]
+      val stdInstance = Class.forName(className).newInstance().asInstanceOf[StdClass]
       if (cInd > 0) stdInstance.setup(pair._2.substring(cInd + 1))
-      (pair._2.substring(0, eInd), stdInstance)
-    });
+      (parseFieldName(pair._2.substring(0, eInd), newMap), stdInstance)
+    })(collection.breakOut):List[((Option[Field], Option[String]), StdClass)];
     
-    
- //   val stdRDD = entityDF.mapPartitions( iter => {
- //       stdBaseClass
- //     } 
- //   )
+    val stdRDD = entityDF.map( entity => {
+      val stdBase = stdBaseInstance(entity)
+      stdFuncObjs.foreach(f => {
+        f._1 match {
+          case (Some(field), options) => {
+            val res = f._2.apply(entity)
+            field.set(stdBase, res)
+          }
+          case (None, _) => {}
+        }
+      })
+      stdBase
+    }).zipWithUniqueId.map(f => (f._2, f._1))
     
     
     //Step 5. persist STD_RDD to K/V store
     
     //Step 6. bucket STD_RDD to multiple BKT_RDD <Long, Set<Long>>
+    
     
     //Step 7. persist BKT_RDD to K/V store
     
@@ -62,6 +94,15 @@ object Loader {
     
     //Step 9. persist linkage
     
+  }
+  
+  def parseFieldName(fieldName: String, fieldMap: Map[String, Field]) = {
+    val beginIndex = fieldName.indexOf('[')
+    if (beginIndex > 0 && fieldName.endsWith("]")) {
+      (fieldMap get fieldName.substring(0, beginIndex), Some(fieldName.substring(beginIndex + 1, fieldName.length - 1)))
+    } else {
+      (fieldMap get fieldName, None)
+    } 
   }
 
 }
--- core/src/main/scala/com/fuseinfo/ber/conf/BerConf.scala
+++ core/src/main/scala/com/fuseinfo/ber/conf/BerConf.scala
@@ -1,7 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.fuseinfo.ber.conf
 
-import java.sql.{Connection,DriverManager}
-import org.apache.spark.{SparkConf, SparkContext}
+import java.sql.{Connection, DriverManager}
+import org.apache.spark.SparkConf
 
 class BerConf(val entity: String, val conf: SparkConf) {
   private val confMap = scala.collection.mutable.Map[String, String]()
@@ -10,9 +29,10 @@ class BerConf(val entity: String, val conf: SparkConf) {
   val confUser   = conf get "ber.confUser"
   val confPass   = conf get "ber.confPass"
   val confSql    = conf get "ber.confSql"
+  var connection:Connection = _
   try {
     Class forName confDriver
-    val connection = DriverManager.getConnection(confUrl, confUser, confPass)
+    connection = DriverManager.getConnection(confUrl, confUser, confPass)
     val statement = connection.createStatement
     val rs = statement.executeQuery(confSql)
     while (rs.next) {
@@ -20,7 +40,10 @@ class BerConf(val entity: String, val conf: SparkConf) {
     }
   } catch {
     case e: Exception => e.printStackTrace
+  } finally {
+    connection.close 
   }
+  
   
   def getValueByKey(key: String) = {
     confMap get key match {
